ef2d9d0b58d2a8a650b69d961dca40074f0556b0
ritesh <riteshswain1973@gmail.com>
2025-10-21 23:33:36 +0530

    Clean The Input

--- backend/src/PlagiarismChecker.java
+++ backend/src/PlagiarismChecker.java
@@ -1,14 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class PlagiarismChecker{
 
-    public static void removePunctuation(String text){
-        text=text.toLowerCase();
-        text=text.replaceAll("[^a-zA-Z0-9\\s]", "");
-    }
-    public static void tokenizeText(String text){
-        String[] tokens = text.split("\\s+");
+    public static Map<String, Object> tokenizeText(String text,int n){
+        Map<String, Object> result = new HashMap<>();
+        text=text.toLowerCase().replaceAll("[^a-z0-9.\\s]", " ").replaceAll("\\s+", " ").trim();
+
+        List<String> words=Arrays.asList(text.split(" "));
+        result.put("words", words);
+
+        
+
+
 
     }
     public static void main(String[] args) {
         String inputFile = "This is a sample text for plagiarism checking.";
+        int n=3; // n-gram size
+        Map<String, Object> tokenizedData = tokenizeText(inputFile, n);
     }
 }
