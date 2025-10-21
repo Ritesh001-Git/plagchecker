434e3a489c192d1ad2bba1b630a54f834fda5dd3
ritesh <riteshswain1973@gmail.com>
2025-10-21 23:35:01 +0530

    Tokenize into word

--- backend/src/PlagiarismChecker.java
+++ backend/src/PlagiarismChecker.java
@@ -8,12 +8,24 @@ public class PlagiarismChecker{
 
     public static Map<String, Object> tokenizeText(String text,int n){
         Map<String, Object> result = new HashMap<>();
+        // --- 1. Clean the text ---
         text=text.toLowerCase().replaceAll("[^a-z0-9.\\s]", " ").replaceAll("\\s+", " ").trim();
 
+        // --- 2. Tokenize into words ---
         List<String> words=Arrays.asList(text.split(" "));
         result.put("words", words);
 
+        String[] sentenceArray = text.split("\\.");
+        List<Integer> sentenceLengths = new ArrayList<>();
+        for(String sentence : sentenceArray){
+            String[]sentenceWords = sentence.trim().split("\\s+");
+            if (sentenceWords.length > 0 && !sentenceWords[0].isEmpty()) {
+                sentenceLengths.add(sentenceWords.length);
+            }
+        }
         
+
+        return result;
 
 
 
