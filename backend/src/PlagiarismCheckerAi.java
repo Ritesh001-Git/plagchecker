0c6a88b6ceecc47c2e39256369c8e051431d24f7
ritesh <riteshswain1973@gmail.com>
2025-10-22 22:33:27 +0530

    Classify AI-ness

--- backend/src/PlagiarismCheckerAi.java
+++ backend/src/PlagiarismCheckerAi.java
@@ -84,6 +84,7 @@ public class PlagiarismCheckerAi {
     public static String classify(double score) {
         if (score < 30) return "Likely Human-written";
         else if (score < 70) return "Possibly Mixed (Human + AI)";
-
+        else return "Likely AI-generated";
+    }
 
 }
