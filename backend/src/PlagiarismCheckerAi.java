7bb5fa6e940bc513406fc60d653a9154286d6947
ritesh <riteshswain1973@gmail.com>
2025-10-22 22:20:02 +0530

    Tokenize paragraph into words

--- backend/src/PlagiarismCheckerAi.java
+++ backend/src/PlagiarismCheckerAi.java
@@ -3,4 +3,6 @@ public class PlagiarismCheckerAi {
     // 1️⃣ Tokenize paragraph into words
     public static List<String> getWords(String text) {
         text = text.toLowerCase().replaceAll("[^a-z0-9\\s]", " ").replaceAll("\\s+", " ").trim();
+        return Arrays.asList(text.split(" "));
+    }
 }
